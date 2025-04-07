3deda12853c2875eba5374055f521fe52a97a9ef
Kaan AKCAN <76076761+KaanAkcan1@users.noreply.github.com>
2025-04-07 14:25:28 +0300

    Add project files.

---
+++ src/OrderService/OrderService.API/Controllers/OrderController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OrderService.API.Controllers
+{
+    [ApiController]
+    [Route("orders")]
+    public class OrderController : ControllerBase
+    {
+        [HttpGet("test")]
+        public IActionResult GetTest()
+        {
+            return Ok("OrderService'den merhaba!");
+        }
+    }
+}
---
+++ src/ProductService/ProductService.API/Controller/ProductController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductService.API.Controller
+{
+    [ApiController]
+    [Route("products")]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet("test")]
+        public IActionResult GetTest()
+        {
+            return Ok("ProductService'den merhaba!");
+        }
+    }
+}
